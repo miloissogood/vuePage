ec24c2fce198711ccddf0c2a5d6c2d7cda84376e
MILO_NB\MILO <miloismilo@gmail.com>
2023-11-06 16:03:50 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,11 @@ set -e
 npm run build
 # 移動至到打包後的dist目錄 
 cd dist
+ls
 git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
 git add -A
 git commit -m 'deploy'
 
-cd -
-git subtree push --prefix dist https://github.com/miloissogood/vuePage.git gh-pages
+git push -f https://github.com/miloissogood/vuePage.git master:gh-pages
 
+cd -
